7a67019e10dc6dde4840f21b1c3a6333ba38d5fc
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-15 15:06:07 +0700

    login.php

---
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Login</title>
+</head>
+<body>
+     <center>
+     <h1>LOGIN</h1>
+     <form action="">
+          <label for="">ID</label>
+          <input type="text"/>
+          <br/>  
+
+          <label for="">Username</label>
+          <input type="text"/>
+          <br/>
+
+          <label for="">Password</label>
+          <input type="password"/>
+          <br/>
+
+          <label for="">Role</label>
+          <select>
+               <option>Admin</option>
+               <option>Siswa</option>
+          </select>
+          <br/>
+
+          <label for="">NIS</label>
+          <input type="text"/>
+          <br/>
+
+          <label for="">Kelas</label>
+          <input type="text"/>
+          <br/>
+          
+          <button>Login</button>
+     </form>
+     </center>
+</body>
+</html>
