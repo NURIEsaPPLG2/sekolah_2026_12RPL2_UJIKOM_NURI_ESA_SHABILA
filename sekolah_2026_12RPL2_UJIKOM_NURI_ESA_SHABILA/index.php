576960f56c97854931bf27b1453f279a68a585cc
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-15 13:39:05 +0700

    Add files via upload

---
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     <h1>SELAMAT DATANG DI WEBSITE PENGADUAN MUTU</h1>
+     <a href="form_pengaduan.php"><button>Buat Pengaduan</button></a>
+     <a href="cari_pengaduan.php"><button>Cari Pengaduan</button></a>
+</body>
+</html>
