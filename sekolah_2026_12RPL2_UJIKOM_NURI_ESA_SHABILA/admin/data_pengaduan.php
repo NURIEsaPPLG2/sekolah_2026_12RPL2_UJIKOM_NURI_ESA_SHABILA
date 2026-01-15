3c44db5a7a2491e0a9a1f7667b191cdc3d1128d7
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-15 15:04:57 +0700

    data_pengaduan.php

---
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/admin/data_pengaduan.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+<body>
+     <h1>DATA PENGADUAN</h1>
+
+     <table border="1">
+          <tr>
+               <th>No</th>
+               <th>Tanggal</th>
+               <th>NIS</th>
+               <th>Kategori</th>
+               <th>Lokasi</th>
+               <th>Keterangan</th>
+               <th>Status</th>
+          </tr>
+</body>
+</html>
