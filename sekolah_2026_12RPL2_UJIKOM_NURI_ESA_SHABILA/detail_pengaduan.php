3a7362a9e18165434b7f6cba67983683a55121f5
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-20 16:59:19 +0700

    detail_pengaduan.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/detail_pengaduan.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/detail_pengaduan.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
-</head>
-<body>
-     
-</body>
-</html>
+<?php
+
+echo $nis = $_POST['nis'];
+// echo $kategori = $_POST['kategori'];
+echo $lokasi = $_POST['lokasi'];
+echo $keterangan = $_POST['keterangan'];
+
+
+$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_nuriesa");
+
+mysqli_query($koneksi, "INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `keterangan`, `status`, `feedback`) 
+VALUES (NULL, '$nis', '1', '$lokasi', '$keterangan', 'proses', NULL);");
+
+?>
