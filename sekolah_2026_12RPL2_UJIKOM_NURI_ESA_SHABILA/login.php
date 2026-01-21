558fa206a02443aa411b6fb2717056d052fd4b80
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-21 16:51:33 +0700

    login.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/login.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/login.php
@@ -8,34 +8,34 @@
 <body>
      <center>
      <h1>LOGIN</h1>
-     <form action="">
-          <label for="">ID</label>
-          <input type="text"/>
-          <br/>  
+     <form action="index.php" method="post">
+          <!-- <label for="">ID</label> -->
+          <!-- <input type="text"/> -->
+          <!-- <br/>   -->
 
           <label for="">Username</label>
-          <input type="text"/>
+          <input type="text" name="username"/>
           <br/>
 
           <label for="">Password</label>
-          <input type="password"/>
+          <input type="password" name="password"/>
           <br/>
 
-          <label for="">Role</label>
-          <select>
-               <option>Admin</option>
-               <option>Siswa</option>
-          </select>
-          <br/>
+          <!-- <label for="">Role</label> -->
+          <!-- <select name="role"> -->
+               <!-- <option value="admin">Admin</option> -->
+               <!-- <option value="siswa">Siswa</option> -->
+          <!-- </select> -->
+          <!-- <br/> -->
 
-          <label for="">NIS</label>
-          <input type="text"/>
-          <br/>
+          <!-- <label for="">NIS</label> -->
+          <!-- <input type="text" name="nis"/> -->
+          <!-- <br/> -->
 
-          <label for="">Kelas</label>
-          <input type="text"/>
-          <br/>
-          
+          <!-- <label for="">Kelas</label> -->
+          <!-- <input type="text" name="kelas"/> -->
+          <!-- <br/> -->
+          <!--  -->
           <button>Login</button>
      </form>
      </center>
