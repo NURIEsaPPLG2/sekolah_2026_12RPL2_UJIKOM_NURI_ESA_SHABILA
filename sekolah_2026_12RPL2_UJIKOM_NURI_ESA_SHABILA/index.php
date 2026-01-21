d62c73ac69acd5ca3d7ed04395af4cbd1e09b51a
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-21 16:52:44 +0700

    index.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/index.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/index.php
@@ -3,13 +3,17 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+     <title>Dashboard</title>
 </head>
 <body>
      <div>
           <div class="judul"><h1>SELAMAT DATANG DI WEBSITE PENGADUAN MUTU</h1></div>
-          <div class="tombol"><a href="form_pengaduan.php"><button>Buat Pengaduan</button></a>
+          <div class="tombol"><a href="form_pengaduan.php"><button>Buat Pengaduan</button></a><br/>
           <a href="cari_pengaduan.php"><button>Cari Pengaduan</button></a></div>
      </div>
 </body>
 </html>
+
+
+
+
