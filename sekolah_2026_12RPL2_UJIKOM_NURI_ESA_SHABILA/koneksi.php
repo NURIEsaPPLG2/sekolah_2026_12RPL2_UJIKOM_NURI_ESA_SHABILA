3a2552f658aaafa7b26dddaf0b30a93124867a11
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-01-28 16:25:09 +0700

    koneksi.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/koneksi.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/koneksi.php
@@ -3,28 +3,7 @@ $koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_nuriesa");
 if(!$koneksi){
     echo "koneksi database gagal";
 }
-// 
-// mysqli_query($koneksi, "INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `keterangan`, `status`, `feedback`) 
-// VALUES (NULL, '111', '1', 'kelas', 'aaaaaaa', 'proses', NULL);");
 
 
 
-
-
-
-
-if (isset($_POST['username'])) {
-
-
-     $nama       =$_POST['username'];
-     $code  =password_hash($_POST['password'],PASSWORD_DEFAULT); 
-     $peran  =$_POST['role'];
-     $nomor   =$_POST['nis'];
-     $ruangan =$_POST['kelas'];
-
-
-$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_nuriesa");
-
-mysqli_query($koneksi,"INSERT INTO user (id, username, password, role, nis, kelas) VALUES (NULL, '$nama', '$code', '$peran', '$nomor', '$ruangan')");
-};
 ?>
