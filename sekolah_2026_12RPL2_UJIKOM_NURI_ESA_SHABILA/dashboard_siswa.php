aef3394e1e602251a762cacc8b8dcacb38e76360
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-09 16:02:51 +0700

    dashboard_siswa.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/dashboard_siswa.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/dashboard_siswa.php
@@ -3,14 +3,55 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Dashboard</title>
+     <title>Dashboard Siswa</title>
+     <style>
+        body{
+            background: #a5d8ff;
+            padding-top: 20px;
+            padding-left: 40px;
+            padding-right: 40px;
+        }         
+         div{
+            height: 100%;
+            width: 100%;
+            background: #a5d8ff;
+         }
+         p{
+            font-size: 50px;
+            font-family: monospace;             
+         }
+         button{
+            width: 180px;
+            height: 70px;
+            background: rgb(72, 72, 255);
+            color: white;
+            border: none;
+            border-radius: 5%;
+            font-family: monospace;
+            font-size: 20px;
+            cursor: pointer;   
+            margin-bottom: 10px;
+         }
+         button:active{
+            background: rgb(158, 158, 255);
+            color: rgb(226, 226, 226);
+        }
+        .logout{
+            float: right;
+            font-family: monospace;
+            color: rgb(255, 46, 46);
+            font-size: 27px;
+        }         
+     </style>
 </head>
 <body>
-     <div>
-          <div class="judul"><h1>SELAMAT DATANG DI WEBSITE PENGADUAN MUTU</h1></div>
-          <div class="tombol"><a href="form_pengaduan.php"><button>Buat Pengaduan</button></a><br/>
-          <a href="cari_pengaduan.php"><button>Cari Pengaduan</button></a></div>
-     </div>
+    <div>
+        <a href="login.php" class="logout">Logout</a><br/>
+        <p>SELAMAT DATANG DI WEBSITE PENGADUAN SARANA SEKOLAH</p>
+        <a href="form_pengaduan.php"><button>Buat Pengaduan</button></a><br/>
+        <a href="cari_pengaduan.php"><button>Data Pengaduan</button></a><br/>
+        <a href="ganti_password.php"><button>Ganti Password</button></a><br/>
+    </div>
 </body>
 </html>
 
@@ -21,6 +62,7 @@ if (!isset($_SESSION['username'])) {
     exit;
 }
 ?>
+
 
 
 
