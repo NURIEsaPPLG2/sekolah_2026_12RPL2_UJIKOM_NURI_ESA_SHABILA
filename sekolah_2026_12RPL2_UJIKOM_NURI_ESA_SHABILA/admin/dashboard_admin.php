236fd008d84d74fbb0bcbd22d8c2b8dece3a72ab
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-09 16:20:28 +0700

    dashboard_admin.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/admin/dashboard_admin.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/admin/dashboard_admin.php
@@ -3,10 +3,56 @@
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
+     <title>Dashboard Admin</title>
+     <style>
+        body{
+            background: #a5d8ff;
+            padding-top: 20px;
+            padding-left: 40px;
+            padding-right: 40px;
+        }                  
+        div{
+            height: 100%;
+            width: 100%;
+            background: #a5d8ff;
+        }
+        p{
+            font-size: 50px;
+            font-family: monospace;             
+        }
+        button{
+            width: 180px;
+            height: 70px;
+            background: rgb(72, 72, 255);
+            color: white;
+            border: none;
+            border-radius: 5%;
+            font-family: monospace;
+            font-size: 20px;
+            cursor: pointer;   
+            margin-bottom: 10px;
+        }
+        button:active{
+            background: rgb(158, 158, 255);
+            color: rgb(226, 226, 226);
+        }              
+        .logout{
+            float: right;
+            font-family: monospace;
+            color: rgb(255, 46, 46);
+            font-size: 27px;
+        }                      
+     </style>
 </head>
 <body>
-     halo admin
+    <div>
+        <a href="login.html" class="logout">Logout</a><br/>
+        <p>SELAMAT DATANG ADMIN DI WEBSITE PENGADUAN SARANA SEKOLAH</p>
+        <a href=""><button>Tambah Data Siswa</button></a><br/>
+        <a href=""><button>Data Siswa</button></a><br/>
+        <a href=""><button>Tambah Kategori</button></a><br/>
+        <a href=""><button>Histori Pengaduan</button></a><br/>
+    </div>
 </body>
 </html>
 
