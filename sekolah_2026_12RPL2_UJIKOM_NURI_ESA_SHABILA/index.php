17a0cc007f6550984ca969bbb00af9ba49974d3c
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-09 14:46:04 +0700

    index.php

---
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>web</title>
+    <style>
+        body{
+            background: #a5d8ff;
+            padding-top: 10px;
+            padding-left: 40px;
+        }
+        div{
+            height: 100%;
+            width: 800px;
+            background: #a5d8ff;
+        }
+        p{
+            font-size: 50px;
+            font-family: monospace;
+        }
+        button{
+            width: 180px;
+            height: 70px;
+            background: rgb(72, 72, 255);
+            color: white;
+            border: none;
+            border-radius: 5%;
+            font-family: monospace;
+            font-size: 30px;
+            cursor: pointer;
+        }
+        button:active{
+            background: rgb(158, 158, 255);
+            color: rgb(226, 226, 226);
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <p>SELAMAT DATANG DI WEBSITE PENGADUAN SARANA SEKOLAH</p>
+        <br>
+        <br>
+        <br>
+        <a href="login.php"><button>LOGIN</button></a>
+    </div>
+</body>
+</html>
