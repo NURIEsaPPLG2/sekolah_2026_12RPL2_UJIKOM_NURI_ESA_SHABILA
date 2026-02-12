9ec61ee61eccd45377f1a84ab3d0c125d2a69f4a
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-12 16:58:39 +0700

    data_pengaduan.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/admin/data_pengaduan.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/admin/data_pengaduan.php
@@ -8,15 +8,42 @@
 <body>
      <h1>DATA PENGADUAN</h1>
 
-     <table border="1">
+     <table border="1" cellpadding="10" cellspacing="0">
           <tr>
                <th>No</th>
-               <th>Tanggal</th>
-               <th>NIS</th>
-               <th>Kategori</th>
+               <th>ID Kategori</th>
+               <th>Nama Kategori</th>
                <th>Lokasi</th>
                <th>Keterangan</th>
                <th>Status</th>
+               <th>Detail</th>
           </tr>
+
+          <?php
+          include '../koneksi.php';
+          $no = 1;
+
+          $query = mysqli_query($koneksi, "SELECT input_aspirasi.*, kategori.ket_kategori
+          FROM input_aspirasi LEFT JOIN kategori ON input_aspirasi.id_kategori = kategori.id_kategori");
+
+          while($data = mysqli_fetch_assoc($query)){
+          ?>
+
+          <tr>
+               <td><?= $no++ ?></td>
+               <td><?= $data['id_kategori'] ?></td>
+               <td><?= $data['ket_kategori'] ?></td>
+               <td><?= $data['lokasi'] ?></td>
+               <td><?= $data['keterangan'] ?></td>
+               <td><?= $data['status'] ?></td>
+               <td>
+                    <a href="detail_pengaduan.php?id=<?= $data['id_pelaporan'] ?>">
+                         <button>Detail</button>
+                    </a>
+               </td>
+          </tr>
+          <?php } ?>
+     </table>
+
 </body>
 </html>
