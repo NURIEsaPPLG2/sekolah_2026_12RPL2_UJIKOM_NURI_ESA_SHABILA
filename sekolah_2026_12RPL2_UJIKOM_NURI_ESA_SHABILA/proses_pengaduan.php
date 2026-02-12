45846c64e993e88eb29e80353253c073615310f1
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-12 16:55:45 +0700

    proses_pengaduan.php

---
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/proses_pengaduan.php
@@ -0,0 +1,20 @@
+<?php
+
+$nis = $_POST['nis'];
+$kategori = $_POST['kategori'];
+$lokasi = $_POST['lokasi'];
+$keterangan = $_POST['keterangan'];
+
+echo "NIS: $nis<br/>";
+echo "Kategori: $kategori<br/>";
+echo "Lokasi: $lokasi<br/>"; 
+echo "Keterangan: $keterangan<br/>";
+
+$koneksi = mysqli_connect("localhost","root","","ujikom_12rpl2_nuriesa");
+
+mysqli_query($koneksi, "INSERT INTO input_aspirasi (id_pelaporan, nis, id_kategori, lokasi, keterangan, status, feedback) 
+VALUES (NULL, '$nis', '$kategori', '$lokasi', '$keterangan', 'proses', NULL)")
+
+
+?>
+
