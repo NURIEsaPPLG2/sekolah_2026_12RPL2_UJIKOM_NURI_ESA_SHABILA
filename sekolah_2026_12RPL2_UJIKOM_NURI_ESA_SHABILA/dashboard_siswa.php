a49af8111ea1e2986bb1adf6e501877c7b91815f
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-19 16:31:36 +0700

    dashboard_siswa.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/dashboard_siswa.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/dashboard_siswa.php
@@ -15,19 +15,18 @@ if (!isset($_SESSION['username'])) {
      <title>Dashboard Siswa</title>
      <style>
         body{
-            background: #a5d8ff;
-            padding-top: 20px;
-            padding-left: 40px;
+            background: linear-gradient(to right, #187bcd, #d0efff);
+            margin: 0;
+            padding-left: 50px;
             padding-right: 40px;
         }         
          div{
             height: 100%;
             width: 100%;
-            background: #a5d8ff;
          }
          p{
             font-size: 50px;
-            font-family: monospace;             
+            font-family: tahoma;             
          }
          button{
             width: 180px;
@@ -40,23 +39,43 @@ if (!isset($_SESSION['username'])) {
             font-size: 20px;
             cursor: pointer;   
             margin-bottom: 10px;
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.5);
          }
          button:active{
             background: rgb(158, 158, 255);
             color: rgb(226, 226, 226);
         }
         .logout{
-            float: right;
             font-family: monospace;
-            color: rgb(255, 46, 46);
+            color: black;
             font-size: 27px;
-        }         
+            text-decoration: none;
+        }    
+        .logout:active{color: white;}
+        img{
+            width: 120px;
+            height: 120px;
+        }       
+        nav{
+            display: flex;
+            align-items: center;      
+            justify-content: space-between; 
+            padding: 0 40px;
+            height: 90px;
+        }            
      </style>
 </head>
 <body>
+    <nav>
+        <img src="https://www.smkmutucikampek.sch.id/wp-content/uploads/2021/06/logo_mutu_png_transparant-removebg-preview-1.png">
+        <a href="login.php" class="logout">Logout</a>
+    </nav>
     <div>
-        <a href="login.php" class="logout">Logout</a><br/>
-        <p>SELAMAT DATANG DI WEBSITE PENGADUAN SARANA SEKOLAH</p>
+        <p>
+            <b>SELAMAT DATANG
+            <?php echo $_SESSION['username']; ?>
+            DI WEBSITE PENGADUAN SARANA SEKOLAH</b>
+        </p>
         <a href="form_pengaduan.php"><button>Buat Pengaduan</button></a><br/>
         <a href="data_pengaduan.php"><button>Data Pengaduan</button></a><br/>
         <a href=""><button>Ganti Password</button></a><br/>
