53d4e5db2a0a3b4f23a25def7e1e9a69e9424c51
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-19 16:33:33 +0700

    form_pengaduan.php

--- sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/form_pengaduan.php
+++ sekolah_2026_12RPL2_UJIKOM_NURI_ESA_SHABILA/form_pengaduan.php
@@ -6,46 +6,70 @@
      <title>Form Pengaduan</title>
      <style>
           body{
-               background: #a5d8ff;
+               background: linear-gradient(to right, #187bcd, #d0efff);
+               height: 100vh;
+               display: flex;
+               align-items: center;
+               justify-content: center;
           }
           div{
                background: #a5d8ff;
-               padding-top: 20px;
+               padding: 20px;
+               width: 400px;
+               height: 450px;
+               display: flex;
+               flex-direction: column;
+               align-items: center;
+               justify-content: center;
+               border-radius: 20px;
+               box-shadow: 0px 4px 6px rgba(0, 0, 0, 3);
+               position: relative;
           }        
           h1{
-               font-family: monospace;
-               font-size: 40px;
+               font-family: tahoma;
+               font-size: 35px;
+               margin-top: 40px;
           }  
           label{
                font-family: monospace;
                font-size: 20px;
           }
           input{
-               margin-bottom: 20px;
+               margin-bottom: 15px;
           }     
           select{
-               margin-bottom: 20px;
+               margin-bottom: 15px;
           }
           button{
-               width: 100px;
+               width: 90px;
                height: 50px;
                background: rgb(72, 72, 255);
                color: white;
                border: none;
                border-radius: 10px;
                font-family: monospace;
-               font-size: 15px;
+               font-size: 20px;
                cursor: pointer;
+               margin-top: 15px;
           }              
           button:active{
                background: rgb(158, 158, 255);
                color: rgb(226, 226, 226);       
           } 
+          a{
+            position: absolute;
+            top: 30px;
+            right: 35px;
+            text-decoration: none;
+            font-family: monospace;
+            font-size: 20px;
+            cursor: pointer;
+          }
      </style>
 </head>
 <body>
-     <center>
      <div>
+          <a href="dashboard_siswa.php">Dashboard</a>
           <h1>BUAT PENGADUAN</h1>
           <form action="proses_pengaduan.php" method="post">
                <label for="">NIS</label><br/>
@@ -65,9 +89,8 @@
                <label for="Keterangan">Keterangan</label><br/>
                <textarea name="keterangan"></textarea>
                <br/>
-               <button>Kirim Pengaduan</button>
+               <button>Kirim</button>
           </form>
      </div>
-     </center>
 </body>
 </html>
