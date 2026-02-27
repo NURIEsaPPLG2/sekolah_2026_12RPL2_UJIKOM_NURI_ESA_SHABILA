1c9a2601a49de0e5c9558abed2a899c3379ce432
NURIEsaPPLG2 <152268447+NURIEsaPPLG2@users.noreply.github.com>
2026-02-27 09:56:38 +0700

    Add files via upload

---
+++ ujikom_12rpl2_nuriesa (2).sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 27, 2026 at 02:55 AM
+-- Server version: 8.4.3
+-- PHP Version: 8.3.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ujikom_12rpl2_nuriesa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `input_aspirasi`
+--
+
+CREATE TABLE `input_aspirasi` (
+  `id_pelaporan` int NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `id_kategori` int NOT NULL,
+  `lokasi` varchar(50) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` enum('menunggu','proses','selesai') NOT NULL DEFAULT 'menunggu',
+  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `feedback` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `input_aspirasi`
+--
+
+INSERT INTO `input_aspirasi` (`id_pelaporan`, `nis`, `id_kategori`, `lokasi`, `keterangan`, `status`, `tanggal`, `feedback`) VALUES
+(28, '30', 1, 'kelas', 'atap bocor', 'selesai', '2026-02-26 08:34:39', 'sudah diperbaiki'),
+(30, '111', 2, 'lapangan voli', 'banjir', 'proses', '2026-02-26 08:38:06', 'sedang diproses'),
+(31, '678', 3, 'kelas', 'bosan', 'menunggu', '2026-02-26 09:00:48', ''),
+(32, '111', 1, 'kelas', 'kuris patah', 'menunggu', '2026-02-26 09:18:49', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int NOT NULL,
+  `ket_kategori` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `ket_kategori`) VALUES
+(1, 'Fasilitas'),
+(2, 'lingkungan'),
+(3, 'pembelajaran');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` enum('siswa','admin') NOT NULL DEFAULT 'siswa',
+  `nis` varchar(10) DEFAULT NULL,
+  `kelas` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`, `nis`, `kelas`) VALUES
+(1, 'winter', '$2y$10$4pc7DYKQWJYyM1vgd1svPubbulwOGkuOKpqmwE15Qu7q6pfrbmMe.', 'siswa', '111', '12 rpl 4'),
+(7, 'admin', '$2y$10$s6o3XYc/cFTQaW63K96q8O8DE9d2mLMB.Sv/N7q3o8yvpYh57e/ie', 'admin', NULL, NULL),
+(9, 'ryujin', '$2y$10$d3uc.83rVJIJQGeTKg17VusL.Zuf7G6gjNgyiF1CRDwmVEGOoqeFC', 'siswa', '678', '12rpl 1'),
+(10, 'minji', '$2y$10$.O/.sFtzytiujxRFrbncseMofUt0HM/CxWnGFNTolosdUP0Y1D/HW', 'siswa', '30', '11 rpl 2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD PRIMARY KEY (`id_pelaporan`),
+  ADD KEY `nis` (`nis`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nis` (`nis`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  MODIFY `id_pelaporan` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `input_aspirasi`
+--
+ALTER TABLE `input_aspirasi`
+  ADD CONSTRAINT `input_aspirasi_ibfk_2` FOREIGN KEY (`nis`) REFERENCES `user` (`nis`),
+  ADD CONSTRAINT `input_aspirasi_ibfk_3` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
